4c29a4d4179d54c510f515aeb42b2c403edf9c68
Soshi31 <157211700+Soshi31@users.noreply.github.com>
2025-04-11 16:14:06 +0300

    Add files via upload

---
+++ xi.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void draw_xi()
+{
+    printf("⣿⣿⣿⣿⣿⠟⠋⠄⠄⠄⠄⠄⠄⠄⢁⠈⢻⢿⣿⣿⣿⣿⣿⣿⣿\n");
+    printf("⣿⣿⣿⣿⣿⠃⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⡀⠭⢿⣿⣿⣿⣿\n");
+    printf("⣿⣿⣿⣿⡟⠄⢀⣾⣿⣿⣿⣷⣶⣿⣷⣶⣶⡆⠄⠄⠄⣿⣿⣿⣿\n");
+    printf("⣿⣿⣿⣿⡇⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠄⠄⢸⣿⣿⣿⣿\n");
+    printf("⣿⣿⣿⣿⣇⣼⣿⣿⠿⠶⠙⣿⡟⠡⣴⣿⣽⣿⣧⠄⢸⣿⣿⣿⣿\n");
+    printf("⣿⣿⣿⣿⣿⣾⣿⣿⣟⣭⣾⣿⣷⣶⣶⣴⣶⣿⣿⢄⣿⣿⣿⣿⣿\n");
+    printf("⣿⣿⣿⣿⣿⣿⣿⣿⡟⣩⣿⣿⣿⡏⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n");
+    printf("⣿⣿⣿⣿⣿⣿⣹⡋⠘⠷⣦⣀⣠⡶⠁⠈⠁⠄⣿⣿⣿⣿⣿⣿⣿\n");
+    printf("⣿⣿⣿⣿⣿⣿⣍⠃⣴⣶⡔⠒⠄⣠⢀⠄⠄⠄⡨⣿⣿⣿⣿⣿⣿\n");
+    printf("⣿⣿⣿⣿⣿⣿⣿⣦⡘⠿⣷⣿⠿⠟⠃⠄⠄⣠⡇⠈⠻⣿⣿⣿⣿\n");
+    printf("⣿⣿⣿⣿⡿⠟⠋⢁⣷⣠⠄⠄⠄⠄⣀⣠⣾⡟⠄⠄⠄⠄⠉⠙⠻\n");
+    printf("⡿⠟⠋⠁⠄⠄⠄⢸⣿⣿⡯⢓⣴⣾⣿⣿⡟⠄⠄⠄⠄⠄⠄⠄⠄\n");
+    printf("⠄⠄⠄⠄⠄⠄⠄⣿⡟⣷⠄⠹⣿⣿⣿⡿⠁⠄⠄⠄⠄⠄⠄⠄⠄\n");
+}
+
+
+int main()
+{
+    printf("Vrei sa-l desenezi pe marele Xi Jinping?\n");
+    sleep(1);
+    printf("Nu conteaza ce vrei, il vom desena pe liderul suprem !!\n");
+    sleep(1);
+    printf("Desenating .....\n\n");
+    sleep(2);
+    draw_xi();
+    printf("\n");
+    sleep(2);
+    printf("Acum fa o plecaciune in fata gloriosului nostru conducator!!\n");
+    sleep(4);
+    printf("NU ai facut plecaciunea. Pentru ofensa adusa partidului comunist\n");
+    printf("chinez ti se vor sustrage 5.000 social credits. GLORY TO THE CCP!!!\n");
+}
